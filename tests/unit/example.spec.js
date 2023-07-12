98977dfc433e9cee9589c2964f67c69b5ac391a5
Brandon Connes <bconnes@ecrs.com>
2023-07-11 21:52:14 -0400

    Vue 3 failing test case with pascalcase stub

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('App.vue', () => {
+  it('finds the helloworld component', () => {
+    const wrapper = shallowMount(App, {})
+    const componentStub = wrapper.find('helloworld-stub');
+    expect(componentStub.html()).toBeTruthy();
   })
 })
